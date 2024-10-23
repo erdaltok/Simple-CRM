e44a1db8d15ec7120253154dd8152dee18cc5a68
Erdal Tok <erdaltok@hotmail.de>
2024-10-24 01:21:23 +0200

    start to generate add user form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,24 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule, RouterLink, MatDialogModule],
+  imports: [RouterOutlet,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    RouterLink,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { MatDialogModule, MatDialogActions, MatDialog} from '@angular/material/dialog';
+import { MatDialogModule, MatDialogActions, MatDialog } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 
 
@@ -7,11 +12,20 @@ import { MatDialogModule, MatDialogActions, MatDialog} from '@angular/material/d
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatDialogModule, MatDialogActions,],
+  imports: [
+    MatDialogModule,
+    MatDialogActions,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatDatepickerModule,
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
+  readonly startDate = new Date(1990, 0, 1);
 
     constructor(public dialog: MatDialog) { }
 
