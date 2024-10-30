635420b0c73190099ee1216a021fda3ac9fc60b5
Erdal Tok <erdaltok@hotmail.de>
2024-10-30 17:33:10 +0100

    include progress bar in dialog component

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
-import { MatDialogModule, MatDialogActions, MatDialog } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import {
+  MatDialogModule,
+  MatDialogActions,
+  MatDialog,
+} from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 import { User } from '../../models/user.class';
 import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 import { inject } from '@angular/core';
-
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -22,39 +27,40 @@ import { inject } from '@angular/core';
     MatFormFieldModule,
     MatButtonModule,
     MatDatepickerModule,
-    FormsModule
+    FormsModule,
+    MatProgressBarModule,
+    CommonModule
   ],
   providers: [provideNativeDateAdapter()],
   templateUrl: './dialog-add-user.component.html',
-  styleUrl: './dialog-add-user.component.scss'
+  styleUrl: './dialog-add-user.component.scss',
 })
 export class DialogAddUserComponent {
   readonly startDate = new Date(1990, 0, 1);
 
-    firestore: Firestore = inject(Firestore);
-
+  firestore: Firestore = inject(Firestore);
   user = new User();
   birthDate?: Date;
+  loading = false;
 
-  constructor(public dialog: MatDialog) { 
-    
-    }
+  constructor(public dialog: MatDialog) {}
 
   saveUser() {
     if (this.birthDate) {
+      this.loading = true;
       const formattedDate = this.birthDate.toLocaleDateString('en-US');
-      this.user.birthDate = formattedDate; 
+      this.user.birthDate = formattedDate;
       console.log('Current user is', this.user);
 
-      addDoc(this.getUsersRef(), this.user.toJSON())
-        .then((result) => {
-          console.log('Adding user finished', result);          
-      })        
-    }         
+      addDoc(this.getUsersRef(), this.user.toJSON()).then((result) => {
+         this.loading = false;
+        console.log('Adding user finished', result);
+      });
+    }
+   
   }
-  
+
   getUsersRef() {
     return collection(this.firestore, 'users');
   }
-
 }
